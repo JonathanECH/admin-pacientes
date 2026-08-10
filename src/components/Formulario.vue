24effbc1203f488d45afe24fa36b615a711382c1
JonathanECH <jonathanecampos3@gmail.com>
2026-08-10 10:13:17 -0400

    Se usó v-model para capturar los datos del form y reactive para crear un objeto llamado "form" donde se almacenan estos datos.

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,7 +1,16 @@
-<script>
-export default {
+<script setup>
+    import { reactive, ref } from 'vue';
+    const form = reactive({
+        mascota: '',
+        propietario: '',
+        email: '',
+        alta: '',
+        sintomas: ''
+    })
 
-}
+    const validarForm = () => {
+        console.log('Validando formulario')
+    }
 </script>
 
 <template lang="">
@@ -11,39 +20,45 @@ export default {
             <span class="text-indigo-600 font-bold">Adminístralos</span>
         </p>
 
-        <form class="bg-white shadow-md rounded-lg p-5">
+        <form class="bg-white shadow-md rounded-lg p-5"
+        @submit.prevent="validarForm">
             <div class="mb-5">
                 <label for="mascota" class="block font-bold text-gray-600 uppercase mt-5">Nombre Mascota</label>
                 <input id="mascota" type="text" class="w-full border-gray-100 rounded-md border-2 p-3
-                placeholder:text-lg placeholder:text-gray-400 placeholder:font-semibold"
-                placeholder="Nombre de la Mascota"></input>
+                placeholder:text-lg placeholder:text-gray-500 placeholder:font-medium"
+                placeholder="Nombre de la Mascota"
+                v-model="form.mascota"></input>
             </div>
 
             <div class="mb-5">
                 <label for="propietario" class="block font-bold text-gray-600 uppercase mt-5">Nombre Propietario</label>
                 <input id="propietario" type="text" class="w-full border-gray-100 rounded-md border-2 p-3
-                placeholder:text-lg placeholder:text-gray-400 placeholder:font-semibold"
-                placeholder="Nombre del Propietario"></input>
+                placeholder:text-lg placeholder:text-gray-500 placeholder:font-medium"
+                placeholder="Nombre del Propietario"   
+                v-model="form.propietario"></input>
             </div>
 
             <div class="mb-5">
                 <label for="email" class="block font-bold text-gray-600 uppercase mt-5">Email</label>
                 <input id="email" type="text" class="w-full border-gray-100 rounded-md border-2 p-3
-                placeholder:text-lg placeholder:text-gray-400 placeholder:font-semibold"
-                placeholder="Email de contacto del Propietario"></input>
+                placeholder:text-lg placeholder:text-gray-500 placeholder:font-medium"
+                placeholder="Email de contacto del Propietario"
+                v-model="form.email"></input>
             </div>
 
             <div class="mb-5">
                 <label for="alta" class="block font-bold text-gray-600 uppercase mt-5">Alta</label>
                 <input id="alta" type="date" class="w-full border-gray-100 rounded-md border-2 p-3 cursor-pointer
-                placeholder:text-lg placeholder:text-gray-400 placeholder:font-semibold"></input>
+                placeholder:text-lg placeholder:text-gray-500 placeholder:font-medium" 
+                v-model="form.alta"></input>
             </div>
 
             <div class="mb-5">
                 <label for="sintomas" class="block font-bold text-gray-600 uppercase mt-5">Síntomas</label>
                 <textarea id="sintomas" class="w-full border-gray-100 rounded-md border-2 p-3 resize-none
-                placeholder:text-lg placeholder:text-gray-400 placeholder:font-semibold"
-                placeholder="Síntomas del Paciente"/>
+                placeholder:text-lg placeholder:text-gray-500 placeholder:font-medium"
+                placeholder="Síntomas del Paciente"
+                v-model="form.sintomas"/>
             </div>
 
             <input type="submit" class="w-full rounded-md mt-5 text-white text-lg uppercase bg-indigo-600 hover:bg-indigo-700 hover:translate-y-1 transition-all duration-300 ease-in-out box-border border border-transparent focus:outline-none font-bold leading-5 px-4 py-2.5 inline-flex items-center justify-center cursor-pointer shadow-md" value="Reguistrar Paciente"></input>
